26f770deaef6776d3eb29802a19f4e65b289d559
Cheng Zhao <chengzhao41@gmail.com>
2016-05-09 02:07:37 -0700

    Initial commit for submission

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""main.py - This file contains handlers that are called by taskqueue and/or
+cronjobs."""
+import logging
+
+import webapp2
+from google.appengine.api import mail, app_identity
+from api import TicTacToeApi
+
+from models import User
+
+
+class SendReminderEmail(webapp2.RequestHandler):
+    def get(self):
+        """Send a reminder email to each User with an email about games.
+        Called every hour using a cron job"""
+        app_id = app_identity.get_application_id()
+        users = User.query(User.email != None and User.email_remainder==True)
+        for user in users:
+            subject = 'This is a reminder!'
+            body = 'Hello {}, try out Tic-Tac-Toe!'.format(user.name)
+            # This will send test emails, the arguments to send_mail are:
+            # from, to, subject, body
+            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+                           user.email,
+                           subject,
+                           body)
+
+
+class UpdateAverageMovesPerGame(webapp2.RequestHandler):
+    def post(self):
+        """Update game listing announcement in memcache."""
+        TicTacToeApi._cache_average_moves_per_game()
+        self.response.set_status(204)
+
+
+app = webapp2.WSGIApplication([
+    ('/crons/send_reminder', SendReminderEmail),
+    ('/tasks/cache_average_moves_per_game', UpdateAverageMovesPerGame),
+], debug=True)
